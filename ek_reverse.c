9af63e3d7b9c1cde629eee1e07c1cf3fc15eac33
ekobak17 <63224714+ekobak17@users.noreply.github.com>
2020-05-19 09:45:03 -0700

    Add files via upload

---
+++ ek_reverse.c
@@ -0,0 +1,43 @@
+/*
+ * Project 5: reverse
+ * Author: Ellie Kobak
+ * usage:
+ *
+ */
+
+#include <stdio.h>
+#include "stringlist.h"
+#include <string.h>
+
+
+int main() {
+	int i = 0;
+	int programError = 1;
+	char input[BUFSIZ];
+	int N;
+	char *str;
+	
+	const StringList *sl = StringList_create(0L);
+	if(sl == NULL){
+		fprintf(stderr, "unable to create String ADT"); 
+		return programError;
+	}
+	while (fgets(input, BUFSIZ, stdin)!= NULL) {
+		if (!sl->append (sl, strdup(input))) {
+			if (sl != NULL) 
+				sl->destroy(sl);
+			fprintf(stderr, "unable to append");
+		}
+	}
+	N = sl->size(sl);
+	for (i = N - 1; i>= 0; i--) {
+		int tmp = sl->get(sl, i, &str);
+		if (tmp)
+			fprintf(stdout, "%s", str);
+	}
+	sl->destroy(sl);
+	
+	return programError;		
+
+}
+
